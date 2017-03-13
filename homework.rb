48cc0a590bcd4a098417827bc2746baac1b5e138
Eun-Joo <eunjooyoon@hotmail.co.uk>
2017-03-13 16:52:55 +0000

    Finished part B of the homework

--- homework.rb
+++ homework.rb
@@ -32,8 +32,47 @@ end
 
 class SportsTeam
 
+attr_accessor :team, :players_array, :coach
 
+def initialize(team, players_array, coach, points)
+  @team = team
+  @players_array = players_array
+  @coach = coach
+  @points = points
+end
 
+# def team_name
+#   return @team
+# end
+
+# def team_players
+#   return @players_array
+# end
+
+# def coach
+#   return @coach
+# end
+
+# def change_coach_name(name)
+#   @coach = name
+# end
+
+def add_player(new_player)
+  @players_array << new_player
+end
+
+def player_in_array?(player)
+  for person in @players_array
+    return true if player == person
+  end
+  return false
+end
+
+def points_for_win(result)
+  if result == "Win"
+    @points += 10
+  end
+end
 
 
 end
--- homework_spec.rb
+++ homework_spec.rb
@@ -33,5 +33,42 @@ end
 # Part B
 class TestSportsTeam < MiniTest::Test
 
+def setup
+  @sports_team = SportsTeam.new("Codeclan", ["Jim", "Bob", "Tom", "George", "Henry"], "Allegra", 0)
+end
+
+def test_team_name
+  assert_equal("Codeclan", @sports_team.team)
+end
+
+def test_team_players
+  assert_equal(["Jim", "Bob", "Tom", "George", "Henry"], @sports_team.players_array)
+end
+
+def test_coach_name
+  assert_equal("Allegra", @sports_team.coach)
+end
+
+def test_change_coach_name
+  @sports_team.coach = "Vodka"
+  assert_equal("Vodka", @sports_team.coach)
+end
+
+def test_add_player
+  @sports_team.add_player("Floofy")
+  assert_equal(["Jim", "Bob", "Tom", "George", "Henry", "Floofy"], @sports_team.players_array)
+end
+
+def test_player_in_array?
+  result = @sports_team.player_in_array?("Bob")
+  assert_equal(true, result)
+end
+
+def test_add_points_for_win
+  result = @sports_team.points_for_win("Win")
+  assert_equal(10, result)
+end
+
+
 
 end
