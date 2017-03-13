83291fb76a19193ffe01b864b7a56c6f64f8e725
Eun-Joo <eunjooyoon@hotmail.co.uk>
2017-03-13 15:07:12 +0000

    First few exercises completed

---
+++ homework.rb
@@ -0,0 +1,23 @@
+class Student
+
+def initialize(student_name, cohort_number)
+  @student_name = student_name
+  @cohort_number = cohort_number
+end
+
+def student_name
+  return @student_name
+end
+
+def cohort_number
+  return @cohort_number
+end
+
+def set_student_name(name)
+  @student_name = name
+end
+
+
+
+
+end
---
+++ homework_spec.rb
@@ -0,0 +1,24 @@
+require ('minitest/autorun')
+require_relative('./homework')
+
+class TestStudent < MiniTest::Test
+
+def setup
+  @student = Student.new("Joo", 11)
+end
+
+def test_student_name
+  assert_equal("Joo", @student.student_name)
+end
+
+def test_cohort_number
+  assert_equal(11, @student.cohort_number)
+end
+
+def test_set_student_name
+  @student.set_student_name("Jooniper")
+  assert_equal("Jooniper", @student.student_name)
+end
+
+
+end
